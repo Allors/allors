f4e57c75728cf9bdf4736dfa7323aaaa38c0de22
Koen Van Exem <koen@vanexem.be>
2022-09-29 10:25:58 +0200

    adapters (WIP)

--- dotnet/System/Database/Adapters/Allors.Database.Adapters/Configuration.cs
+++ dotnet/System/Database/Adapters/Allors.Database.Adapters/Configuration.cs
@@ -1,4 +1,4 @@
-// <copyright file="Configuration.cs" company="Allors bvba">
+﻿// <copyright file="Configuration.cs" company="Allors bvba">
 // Copyright (c) Allors bvba. All rights reserved.
 // Licensed under the LGPL license. See LICENSE file in the project root for full license information.
 // </copyright>
@@ -9,7 +9,4 @@ public abstract class Configuration
 {
     public IObjectFactory ObjectFactory { get; set; }
 
-    public IRoleCache RoleCache { get; set; }
-
-    public IClassCache ClassCache { get; set; }
 }
