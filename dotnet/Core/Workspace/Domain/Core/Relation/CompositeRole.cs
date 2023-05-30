94b574c28f2c2fab4ded6f4c4a901d4fbdc7492f
Koen Van Exem <koen@vanexem.be>
2023-05-30 11:20:34 +0200

    workspace (WIP)

---
+++ dotnet/Core/Workspace/Domain/Core/Relation/CompositeRole.cs
@@ -0,0 +1,57 @@
+﻿// <copyright file="Object.cs" company="Allors bvba">
+// Copyright (c) Allors bvba. All rights reserved.
+// Licensed under the LGPL license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace Allors.Workspace
+{
+    using Meta;
+
+    public class CompositeRole<T> : ICompositeRole where T : class, IObject
+    {
+        public CompositeRole(IStrategy strategy, IRoleType roleType)
+        {
+            this.Object = strategy;
+            this.RoleType = roleType;
+        }
+
+        public IStrategy Object { get; }
+
+        public IRelationType RelationType => this.RoleType.RelationType;
+
+        public IRoleType RoleType { get; }
+
+        object IRelationEnd.Value => this.Value;
+
+        object IRole.Value
+        {
+            get => this.Value;
+            set => this.Value = (T)value;
+        }
+
+        IStrategy ICompositeRole.Value
+        {
+            get => this.Object.GetCompositeRole(this.RoleType);
+            set => this.Object.SetCompositeRole(this.RoleType, value);
+        }
+
+        T Value
+        {
+            get => (T)this.Object.GetCompositeRole(this.RoleType);
+            set => this.Object.SetCompositeRole(this.RoleType, value.Strategy);
+        }
+        
+        public bool CanRead => this.Object.CanRead(this.RoleType);
+
+        public bool CanWrite => this.Object.CanWrite(this.RoleType);
+
+        public bool Exist => this.Object.ExistRole(this.RoleType);
+
+        public bool IsModified => this.Object.IsModified(this.RoleType);
+
+        public void Restore()
+        {
+            this.Object.RestoreRole(this.RoleType);
+        }
+    }
+}
