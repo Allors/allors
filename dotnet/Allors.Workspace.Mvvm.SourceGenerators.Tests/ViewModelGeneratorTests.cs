6c080d1724618ebd7e34c9707ba63b50b74a669c
Nowipi <noah.uyttebroeck@gmail.com>
2023-08-03 09:40:18 +0200

    SourceGenerationTest WIP

--- dotnet/Allors.Workspace.Mvvm.SourceGenerators.Tests/ViewModelGeneratorTests.cs
+++ dotnet/Allors.Workspace.Mvvm.SourceGenerators.Tests/ViewModelGeneratorTests.cs
@@ -1,22 +1,19 @@
 ﻿namespace Allors.Workspace.Mvvm.SourceGenerators.Tests
 {
     using System.Reflection;
-    using System.Text;
     using Generator;
     using Microsoft.CodeAnalysis;
     using Microsoft.CodeAnalysis.CSharp;
-    using Microsoft.CodeAnalysis.Text;
-    using NUnit.Framework.Internal;
-    using VerifyCS = CSharpSourceGeneratorVerifier<Generator.ViewModelGenerator>;
 
     public class ViewModelGeneratorTests
     {
 
         [Test]
-        public async Task Test1()
+        public void GenerateBasicViewModelTest()
         {
-            var code = $@"
+            var source = $@"
 using System;
+using Allors.Workspace.Mvvm;
 
 namespace Test;
 
@@ -27,7 +24,7 @@ public partial class TestClass
 }}
 
 ";
-            var generated = $@"
+            var expected = $@"
 using System;
 
 namespace Test;
@@ -43,17 +40,43 @@ public partial class TestClass
 }}
 
 ";
-            await new VerifyCS.Test
+
+            string output = GetGeneratedOutput(source);
+
+            Assert.NotNull(output);
+
+            Assert.AreEqual(expected, output.Trim());
+        }
+
+        private string GetGeneratedOutput(string source)
+        {
+            var syntaxTree = CSharpSyntaxTree.ParseText(source);
+
+            var references = new List<MetadataReference>();
+            Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            foreach (var assembly in assemblies)
             {
-                TestState =
+                if (!assembly.IsDynamic && !string.IsNullOrWhiteSpace(assembly.Location))
                 {
-                    Sources = { code },
-                    GeneratedSources =
-                    {
-                        (typeof(ViewModelGenerator), "TestClass.g.cs", SourceText.From(generated, Encoding.UTF8, SourceHashAlgorithm.Sha256)),
-                    },
-                },
-            }.RunAsync();
+                    references.Add(MetadataReference.CreateFromFile(assembly.Location));
+                }
+            }
+
+            var compilation = CSharpCompilation.Create("foo", new SyntaxTree[] { syntaxTree }, references, new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+            // TODO: Uncomment this line if you want to fail tests when the injected program isn't valid _before_ running generators
+            var compileDiagnostics = compilation.GetDiagnostics();
+            Assert.False(compileDiagnostics.Any(d => d.Severity == DiagnosticSeverity.Error), "Failed: " + compileDiagnostics.FirstOrDefault()?.GetMessage());
+
+            ISourceGenerator generator = new ViewModelGenerator();
+
+            var driver = CSharpGeneratorDriver.Create(generator);
+            driver.RunGeneratorsAndUpdateCompilation(compilation, out var outputCompilation, out var generateDiagnostics);
+            Assert.False(generateDiagnostics.Any(d => d.Severity == DiagnosticSeverity.Error), "Failed: " + generateDiagnostics.FirstOrDefault()?.GetMessage());
+
+            string output = outputCompilation.SyntaxTrees.Last().ToString();
+
+            return output;
         }
     }
 }
