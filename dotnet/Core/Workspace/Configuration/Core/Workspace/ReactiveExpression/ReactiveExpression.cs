42a4d0b85141a8c92bb71f800da7ca1dcd2791ad
Koen Van Exem <koen@dipu.com>
2023-08-09 14:06:25 +0200

    workspace (WIP)

--- dotnet/Core/Workspace/Configuration/Core/Workspace/ReactiveExpression/ReactiveExpression.cs
+++ dotnet/Core/Workspace/Configuration/Core/Workspace/ReactiveExpression/ReactiveExpression.cs
@@ -6,27 +6,55 @@
 namespace Allors.Workspace.Configuration
 {
     using System;
+    using System.Collections.Generic;
     using System.ComponentModel;
     using System.Linq.Expressions;
 
     public class ReactiveExpression<TObject, TValue> : IReactiveExpression<TValue>
     {
-        private readonly Func<TObject, TValue> expression;
+        private readonly Func<TObject, HashSet<INotifyPropertyChanged>, TValue> expression;
         private readonly TObject @object;
+
+        private HashSet<INotifyPropertyChanged> dependencies;
+        private TValue value;
 
         public event PropertyChangedEventHandler PropertyChanged;
 
         public ReactiveExpression(LambdaExpression expression, TObject @object)
         {
             this.@object = @object;
-            this.expression = (Func<TObject, TValue>)expression.Compile();
+            this.expression = (Func<TObject, HashSet<INotifyPropertyChanged>, TValue>)expression.Compile();
         }
 
         public TValue Value
         {
             get
             {
-                return this.expression(this.@object);
+                if (this.dependencies == null)
+                {
+                    this.dependencies = new HashSet<INotifyPropertyChanged>();
+                    this.value = this.expression(this.@object, this.dependencies);
+
+                    foreach (var dependency in this.dependencies)
+                    {
+                        dependency.PropertyChanged += this.Dependency_PropertyChanged;
+                    }
+                }
+
+                return this.value;
+            }
+        }
+
+        private void Dependency_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (this.dependencies != null)
+            {
+                foreach (var source in this.dependencies)
+                {
+                    source.PropertyChanged -= this.Dependency_PropertyChanged;
+                }
+
+                this.dependencies = null;
             }
         }
     }
--- dotnet/Core/Workspace/Configuration/Core/Workspace/ReactiveExpression/ReactiveVisitor.cs
+++ dotnet/Core/Workspace/Configuration/Core/Workspace/ReactiveExpression/ReactiveVisitor.cs
@@ -5,9 +5,34 @@
 
 namespace Allors.Workspace.Configuration
 {
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Linq;
     using System.Linq.Expressions;
 
     public class ReactiveVisitor : ExpressionVisitor
     {
+        private ParameterExpression dependencies;
+
+        protected override Expression VisitLambda<T>(Expression<T> node)
+        {
+            this.dependencies = Expression.Parameter(typeof(HashSet<INotifyPropertyChanged>), "_dependencies_");
+            var body = this.Visit(node.Body);
+            return Expression.Lambda(body, node.Name, node.Parameters.Append(this.dependencies));
+        }
+
+        protected override Expression VisitMember(MemberExpression node)
+        {
+            var member = node.Member;
+            var declaringType = member.DeclaringType;
+
+            if (declaringType.GetInterfaces().Contains(typeof(INotifyPropertyChanged)))
+            {
+                Console.WriteLine(0);
+            }
+
+            return base.VisitMember(node);
+        }
     }
 }
