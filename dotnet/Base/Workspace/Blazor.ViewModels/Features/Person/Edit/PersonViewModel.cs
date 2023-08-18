cb92f959410d371f08715b67b2f18235a48da25a
Koen Van Exem <koen@vanexem.be>
2023-08-18 16:57:32 +0200

    workspace (WIP)

--- dotnet/Base/Workspace/Blazor.ViewModels/Features/Person/Edit/PersonViewModel.cs
+++ dotnet/Base/Workspace/Blazor.ViewModels/Features/Person/Edit/PersonViewModel.cs
@@ -1,6 +1,8 @@
 ﻿namespace Workspace.Blazor.ViewModels.Features.Person.Edit;
 
+using System.Linq.Expressions;
 using Allors.Workspace;
+using Allors.Workspace.Domain;
 using Allors.Workspace.Meta;
 using Allors.Workspace.Mvvm;
 using Allors.Workspace.Mvvm.Adapters;
@@ -11,44 +13,29 @@ using Person = Allors.Workspace.Domain.Person;
 public partial class PersonViewModel : ViewModel<Person>
 {
     [AdapterProperty] private readonly UnitRoleAdapter<string> firstName;
-    [AdapterProperty] private readonly UnitRoleExpressionAdapter<string> poBox;
-    private readonly ExpressionAdapter<string> fullName;
+    [AdapterProperty] private readonly UnitRoleExpressionAdapter<Person, string> poBox;
+    private readonly ExpressionAdapter<Person, string> fullName;
     private readonly GreetingAdapter greeting;
 
     public PersonViewModel(Person model)
     {
         this.Model = model;
 
-        var myWeakReference = new WeakReference<IViewModel>(this);
+        var workspace = model.Strategy.Workspace;
+        var reactiveFuncBuilder = workspace.Services.Get<IReactiveFuncBuilder>();
+        var reactiveExpressionBuilder = workspace.Services.Get<IReactiveExpressionBuilder>();
 
         this.firstName = new UnitRoleAdapter<string>(this, model.FirstName);
-        this.fullName = new ExpressionAdapter<string>(this,
-            new IRole[]
-            {
-                model.FirstName,
-                model.LastName
-            }, () => model.FirstName.Value + " " + model.LastName, "FullName");
+        this.fullName = new ExpressionAdapter<Person, string>(this, reactiveExpressionBuilder.Build(model, reactiveFuncBuilder.Build((Expression<Func<Person, string>>)(v => v.FirstName.Value + " " + v.LastName.Value))), "FullName");
         this.greeting = new GreetingAdapter(model, this);
-        this.poBox = model.PathAdapter<string, IMetaPerson>(v => v.MailboxAddress.ObjectType.PoBox, this, nameof(this.poBox));
+        this.poBox = new UnitRoleExpressionAdapter<Person, string>(this, reactiveExpressionBuilder.Build(model, reactiveFuncBuilder.Build((Expression<Func<Person, IUnitRole<String>>>)(v => v.MailboxAddress.Value.PoBox))), "PoBox");
 
         var temp = this.FirstName;
     }
 
     public override Person Model { get; }
 
-    //public string FirstName
-    //{
-    //    get => this.firstName.Value;
-    //    set => this.firstName.Value = value;
-    //}
-
     public string FullName => this.fullName.Value;
 
     public string Greeting => this.greeting.Value;
-
-    //public string PoBox
-    //{
-    //    get => this.poBox.Value;
-    //    set => this.poBox.Value = value;
-    //}
 }
