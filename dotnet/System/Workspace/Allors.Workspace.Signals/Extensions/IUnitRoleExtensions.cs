d1105a081dc4a3545ed870ec0fe6cd970aa90ee4
Koen Van Exem <koen@vanexem.be>
2023-09-01 12:06:09 +0200

    workspace (WIP)

---
+++ dotnet/System/Workspace/Allors.Workspace.Signals/Extensions/IUnitRoleExtensions.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="Object.cs" company="Allors bvba">
+// Copyright (c) Allors bvba. All rights reserved.
+// Licensed under the LGPL license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace Allors.Workspace
+{
+    public static class ICompositeRoleExtensions
+    {
+        public static T TrackedValue<T>(this ICompositeRole<T> @this, ITracker tracker)
+            where T : class, IObject
+        {
+            tracker.Track(@this);
+            return @this.Value;
+        }
+    }
+}
