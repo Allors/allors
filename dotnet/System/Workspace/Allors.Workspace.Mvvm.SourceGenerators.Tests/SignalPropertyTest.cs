893c4a74dd014965ece61c108190ce6908219291
Joren Schelkens <joren.schelkens@gmail.com>
2023-09-08 08:34:07 +0200

    workspace (WIP)

--- dotnet/System/Workspace/Allors.Workspace.Mvvm.SourceGenerators.Tests/SignalPropertyTest.cs
+++ dotnet/System/Workspace/Allors.Workspace.Mvvm.SourceGenerators.Tests/SignalPropertyTest.cs
@@ -111,6 +111,115 @@ public partial class TestClass
         }
 
         [Test]
+        public void ComputedNullableUnitRole()
+        {
+            var source = @"
+using System;
+using Allors.Workspace;
+using Allors.Workspace.Signals;
+using Allors.Workspace.Mvvm.Generator;
+
+namespace Signal.Test;
+
+public partial class TestClass
+{
+    [SignalProperty] private readonly IComputedSignal<IUnitRole<string>?> firstName;
+
+}
+";
+
+            var expected =
+                @"using System;
+using Allors.Workspace.Signals;
+using Allors.Workspace;
+
+namespace Signal.Test;
+
+public partial class TestClass
+{
+    private IEffect firstNameChanged;
+
+    public IUnitRole<string>? FirstName
+    {
+        get => this.firstName.Value?.Value;
+        set => this.firstName.Value.Value = value;
+    }
+
+    private void OnInitEffects(IDispatcher dispatcher)
+    {
+        this.firstNameChanged = dispatcher.CreateEffect(tracker => this.firstName.Track(tracker).Value?.Track(tracker), () => this.OnPropertyChanged(nameof(FirstName)));
+    }
+
+    private void OnDisposeEffects()
+    {
+        this.firstNameChanged?.Dispose();
+    }
+}
+".ReplaceLineEndings("\n");
+
+            string output = GetGeneratedOutput(source).ReplaceLineEndings("\n");
+
+            Assert.NotNull(output);
+
+            Assert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void ComputedUnitRoleWithDisplayName()
+        {
+            var source = @"
+using System;
+using Allors.Workspace;
+using Allors.Workspace.Signals;
+using Allors.Workspace.Mvvm.Generator;
+
+namespace Signal.Test;
+
+public partial class TestClass
+{
+    [SignalProperty(""Test Name"")] private readonly IComputedSignal<IUnitRole<string?>> firstName;
+
+}
+";
+
+            var expected =
+                @"using System;
+using Allors.Workspace.Signals;
+using Allors.Workspace;
+
+namespace Signal.Test;
+
+public partial class TestClass
+{
+    private IEffect firstNameChanged;
+
+    [DisplayName(""Test Name"")]
+    public string? FirstName
+    {
+        get => this.firstName.Value?.Value;
+        set { if(this.firstName.Value != null) this.firstName.Value.Value = value; }
+    }
+
+    private void OnInitEffects(IDispatcher dispatcher)
+    {
+        this.firstNameChanged = dispatcher.CreateEffect(tracker => this.firstName.Track(tracker).Value?.Track(tracker), () => this.OnPropertyChanged(nameof(FirstName)));
+    }
+
+    private void OnDisposeEffects()
+    {
+        this.firstNameChanged?.Dispose();
+    }
+}
+".ReplaceLineEndings("\n");
+
+            string output = GetGeneratedOutput(source).ReplaceLineEndings("\n");
+
+            Assert.NotNull(output);
+
+            Assert.AreEqual(expected, output);
+        }
+
+        [Test]
         public void ComputedString()
         {
             var source = @"
@@ -142,6 +251,60 @@ public partial class TestClass
     public string? FullName
     {
         get => this.fullName.Value;
+    }
+
+    private void OnInitEffects(IDispatcher dispatcher)
+    {
+        this.fullNameChanged = dispatcher.CreateEffect(tracker => this.fullName.Track(tracker), () => this.OnPropertyChanged(nameof(FullName)));
+    }
+
+    private void OnDisposeEffects()
+    {
+        this.fullNameChanged?.Dispose();
+    }
+}
+".ReplaceLineEndings("\n");
+
+            string output = GetGeneratedOutput(source).ReplaceLineEndings("\n");
+
+            Assert.NotNull(output);
+
+            Assert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void ComputedViewModel()
+        {
+            var source = @"
+using System;
+using Allors.Workspace;
+using Allors.Workspace.Signals;
+using Allors.Workspace.Mvvm.Generator;
+
+namespace Signal.Test;
+
+public partial class TestClass
+{
+    [SignalProperty] private readonly IComputedSignal<PersonViewModel?> person;
+
+}
+";
+
+            var expected =
+                @"using System;
+using Allors.Workspace.Signals;
+using Allors.Workspace;
+
+namespace Signal.Test;
+
+public partial class TestClass
+{
+    private IEffect fullNameChanged;
+
+    public PersonViewModel? Person
+    {
+        get => this.person.Value;
+        set => this.person.Value = value;
     }
 
     private void OnInitEffects(IDispatcher dispatcher)
