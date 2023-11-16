b97538604718b8cb0a8cc17fd6187c98f2f5281f
Koen Van Exem <koen@dipu.com>
2023-11-16 15:51:46 +0100

    update externals

--- dotnet/Core/Database/Domain/Core/Extensions/IObjectExtensions.cs
+++ dotnet/Core/Database/Domain/Core/Extensions/IObjectExtensions.cs
@@ -1,4 +1,4 @@
-// <copyright file="ObjectExtensions.cs" company="Allors bvba">
+﻿// <copyright file="ObjectExtensions.cs" company="Allors bvba">
 // Copyright (c) Allors bvba. All rights reserved.
 // Licensed under the LGPL license. See LICENSE file in the project root for full license information.
 // </copyright>
@@ -7,6 +7,6 @@ namespace Allors.Database.Domain
 {
     public static class IObjectExtensions
     {
-        public static ITransaction Transaction(this IObject @this) => @this.Transaction();
+        public static ITransaction Transaction(this IObject @this) => @this.Strategy.Transaction;
     }
 }
