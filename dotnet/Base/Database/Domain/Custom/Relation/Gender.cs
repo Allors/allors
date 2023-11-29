e1d42600ffde529866ce5f2c11e7d5fd5a3e1e62
Koen Van Exem <koen@vanexem.be>
2023-11-29 16:56:03 +0100

    fix build

--- dotnet/Base/Database/Domain/Custom/Relation/Gender.cs
+++ dotnet/Base/Database/Domain/Custom/Relation/Gender.cs
@@ -10,7 +10,7 @@ namespace Allors.Database.Domain
     public partial class Gender
     {
         public static readonly Guid MaleId = new Guid("DAB59C10-0D45-4478-A802-3ABE54308CCD");
-        public static readonly Guid FemaleId = new Guid("");
+        public static readonly Guid FemaleId = new Guid("B36DD125-0C6C-47D2-83C3-A04CA6F80A35");
         public static readonly Guid OtherId = new Guid("09210D7C-804B-4E76-AD91-0E150D36E86E");
         public static readonly Guid PreferNotToSayId = new Guid("AEE7F928-B36B-47AE-BB17-747F1D0A9D23");
     }
