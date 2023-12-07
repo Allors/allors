69ef6c6db12d8ef32d07f9526e0a1511567c57e3
Koen Van Exem <koen@dipu.com>
2023-12-07 13:59:25 +0100

    population (WIP)

--- dotnet/Base/Database/Population/Custom/FixtureFile.cs
+++ dotnet/Base/Database/Population/Custom/FixtureFile.cs
@@ -4,6 +4,7 @@
     using System.Collections.Generic;
     using System.Linq;
     using Database;
+    using Database.Meta;
     using Database.Population;
     using Database.Roundtrip;
 
@@ -11,17 +12,28 @@
     {
         private Func<IStrategy, Handle> HandleResolver()
         {
-            Func<IStrategy, Handle> handleResolver = _ => null;
-
+            var excluded = new HashSet<IClass> { this.m.Country };
             var fromExisting = HandleResolvers.FromFixture(this.ExistingFixture);
             var fromKey = HandleResolvers.PascalCaseKey();
-            handleResolver = strategy => fromExisting(strategy) ?? fromKey(strategy);
-            return handleResolver;
+
+            return strategy =>
+            {
+                if (excluded.Contains(strategy.Class))
+                {
+                    return null;
+                }
+
+                return fromExisting(strategy) ?? fromKey(strategy);
+            };
         }
 
         private IEnumerable<IObject> Objects(ITransaction transaction)
         {
-            return this.ExistingFixture.RecordsByClass.Keys
+            var extraClasses = new IClass[] { this.m.Country };
+            var existingClasses = this.ExistingFixture.RecordsByClass.Keys;
+            var classes = extraClasses.Union(existingClasses).Distinct();
+
+            return classes
                 .SelectMany(transaction.Extent);
         }
     }
