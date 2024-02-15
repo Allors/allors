a1fb38badfb58ba0fec47ff136aa0e3efd97322a
Koen Van Exem <koen@dipu.com>
2024-02-15 15:48:11 +0100

    tree (WIP)

--- dotnet/Base/Database/Domain.Tests/Tests/Data/TreeBuilderTests.cs
+++ dotnet/Base/Database/Domain.Tests/Tests/Data/TreeBuilderTests.cs
@@ -8,56 +8,8 @@
 
 namespace Allors.Database.Domain.Tests
 {
-    using System.Collections.Generic;
-    using System.Linq;
-    using Database.Data;
-    using Meta;
     using Xunit;
-
-    public record OrganizationTreeBuilder : ITreeBuilder
-    {
-        public OrganizationOwnerNodeBuilder Owner { get; init; }
-
-        public Node[] Build()
-        {
-            return new[] { this.Owner?.Build() }.Where(v => v != null).ToArray();
-        }
-    }
-
-    public record PersonTreeBuilder : ITreeBuilder
-    {
-        public PersonFirstNameNodeBuilder FirstName { get; init; }
-
-        public Node[] Build()
-        {
-            IEnumerable<Node> nodes = [
-                this.FirstName?.Build()
-            ];
-            return nodes.Where(v => v != null).ToArray();
-        }
-    }
-
-    public record OrganizationOwnerNodeBuilder(M M) : INodeBuilder
-    {
-        public PersonTreeBuilder Person { get; init; }
-
-        public Node Build()
-        {
-            IEnumerable<Node>[] nodes = [
-                this.Person?.Build()
-            ];
-            return new Node(this.M.Organization.Owner, nodes.Where(v => v != null).SelectMany(v => v));
-        }
-    }
-
-    public record PersonFirstNameNodeBuilder(M M) : INodeBuilder
-    {
-        public Node Build()
-        {
-            return new Node(this.M.Person.FirstName);
-        }
-    }
-
+   
     public class TreeBuilderTests(Fixture fixture) : DomainTest(fixture), IClassFixture<Fixture>
     {
         [Fact]
